4d8556f6d2e1a85146f8f15be90490f7e0f5faaa
sonnetsaif <islam.saiful03@outlook.com>
2024-10-02 00:37:41 +0600

    update

--- aop-concept/src/main/java/io/sonnetsaif/aop_concept/AopConceptApplication.java
+++ aop-concept/src/main/java/io/sonnetsaif/aop_concept/AopConceptApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class AopConceptApplication {
 
 	public static void main(String[] args) {
+//		ApplicationContext context = SpringApplication.run(AopConceptApplication.class, args);
+//		context.getBean(LoggingAspect.class);
 		ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 		PaymentService paymentService = context.getBean(PaymentService.class);
 		paymentService.makeDebitPayment("Input");
